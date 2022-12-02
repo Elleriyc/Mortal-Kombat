7effd483027b1a93895c546ceb5fe42ce6d63f36
Malik rayane <rayane@pop-os.localdomain>
2022-12-02 23:37:09 +0100

    indent valides

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ const subKamehaLoop = Array.from(Array(13).keys())
 const subKickLoop = [0,1,2,3,4];
 const subOneTwoLoop = [0,1,2,3,4,5];
 const subRunLoop = [0,1,2,3,4,5];
-const subWalkingLoop = [0,1,2,3,4,5,6,7];
+const subWalkingingLoop = [0,1,2,3,4,5,6,7];
 
 
 let keyPresses = {};
@@ -23,9 +23,34 @@ let frameCount = 0;
 let positionX = 0;
 let positionY = 0;
 
+let x = 0;
 
-let img = new Image();
-let subWalk = new Image();
+let subHurt = new Image();
+let subIdle = new Image();
+let subKameha = new Image();
+let subKick = new Image();
+let subOneTwo = new Image();
+let subRun = new Image();
+let subWalking = new Image();
+
+var time = 0;
+var time_framerate = 100; //in milliseconds'
+
+function loadImage() {
+
+     subHurt.src = "/src/SubZero/Sub_hurt.png"
+     subIdle.src = "/src/SubZero/Sub_idle.png";
+     subKameha.src = "/src/SubZero/Sub_Kameha.png"
+     subKick.src = "/src/SubZero/Sub_kick.png"
+     subOneTwo.src = "/src/SubZero/Sub_OneTwo.png"
+     subRun.src = "/src/SubZero/Sub_Run.png"
+     subWalking.src = "/src/SubZero/Sub_Walk.png";
+
+
+     subHurt.onload = function() {
+     window.requestAnimationFrame(gameLoop);
+     }
+}
 
 let x = 0;
 
@@ -39,77 +64,56 @@ var time_framerate = 100; //in milliseconds'
 
 window.addEventListener('keydown', keyDownListener);
 function keyDownListener(event) {
-    keyPresses[event.key] = true;
+     keyPresses[event.key] = true;
 }
 
 window.addEventListener('keyup', keyUpListener);
 function keyUpListener(event) {
-    keyPresses[event.key] = false;
-}
-
-function loadImage() {
-  subWalk.src = "/src/SubZero/Sub_Walk.png";
-  subWalk.onload = function() {
-    window.requestAnimationFrame(gameLoop);
-  };
-
-  img.src = "/src/SubZero/Sub_idle.png";
-     img.onload = function() {
-       window.requestAnimationFrame(gameLoop);
-     };
+     keyPresses[event.key] = false;
 }
 
 
 
-/*function drawFrame(image,largeur, frameX, frameY, canvasX, canvasY) {
-  ctx.drawImage(image,
-                frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
-                canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
-}*/
-function drawFrame(img, largeur, frameX, frameY/*canvasX*/,canvasY){
-     ctx.drawImage(img, frameX * largeur, frameY * HEIGHT, largeur, HEIGHT, /*canvas*/x, canvasY, SCALE * largeur, SCALED_HEIGHT);
- }
+function drawFrame(image, largeur, frameX, frameY,canvasY){
+     ctx.drawImage(image, frameX * largeur, frameY * HEIGHT, largeur, HEIGHT, /*canvas*/x, canvasY, SCALE * largeur, SCALED_HEIGHT);
+}
 
 loadImage();
 
 function gameLoop(timestamp) {
      if(timestamp > time + time_framerate) {
           time = timestamp;
-     
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  let hasMoved = false;
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (keyPresses.a) {
-    //moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT);
-    x -= 10;
-    hasMoved = true;
-  } else if (keyPresses.d) {
-    //moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
-    x += 10;
-    hasMoved = true;
-  }
+          let hasMoved = false;
 
-  if (hasMoved) {
-      currentLoopIndex++;
-      if (currentLoopIndex >= subWalkingLoop.length) {
-        currentLoopIndex = 0;
-    }
-    drawFrame(subWalk,48,subWalkingLoop[currentLoopIndex], 0, 0);
-  }
+          if (keyPresses.a) {
+               x -= 10;
+               hasMoved = true;
+          } else if (keyPresses.d) {
+               x += 10;
+               hasMoved = true;
+          }
 
-  if (!hasMoved) {
-          currentLoopIndex++;
-          if (currentLoopIndex >= subIdleLoop.length) {
-            currentLoopIndex = 0;
+          if (hasMoved) {
+               currentLoopIndex++;
+               if (currentLoopIndex >= subIdleLoop.length) {
+                    currentLoopIndex = 0;
+          }
+               drawFrame(subIdle,48,subIdleLoop[currentLoopIndex], 0, 0);
+          }
+
+          if (!hasMoved) {
+               currentLoopIndex++;
+               if (currentLoopIndex >= subRunLoop.length) {
+                    currentLoopIndex = 0;
+               }
+               drawFrame(subRun, 48,subRunLoop[currentLoopIndex], 0 , 0 );
+          }
      }
-    drawFrame(img, 48,subIdleLoop[currentLoopIndex], 0 , 0 );
-  }
-}
 
-
-  
-  window.requestAnimationFrame(gameLoop);
+     window.requestAnimationFrame(gameLoop);
 }
 //setInterval(gameLoop, 2000);
 
